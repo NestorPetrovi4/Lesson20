a1908ee96a28f24b9cd7ca85987a8ca0398a07ed
Oleg Nesterov <oleg9988@rambler.ru>
2022-10-18 13:22:11 +0300

    Созданы стримы для получения различной информации

--- src/Main.java
+++ src/Main.java
@@ -15,14 +15,26 @@ public class Main {
                     Education.values()[new Random().nextInt(Education.values().length)])
             );
         }
-        Long personJunior = persons.stream().
+        Long countJunior = persons.stream().
                 filter(p -> p.getAge() < 18).
                 count();
-        System.out.println("Количество несовершеннолетних = " + personJunior);
+        System.out.println("Количество несовершеннолетних = " + countJunior);
+
         List<String> personArmy = persons.stream().
-                filter(p-> p.getAge() >= 18 && p.getAge() < 27 && p.getSex() == Sex.MAN).
+                filter(p -> p.getAge() >= 18 && p.getAge() < 27 && p.getSex() == Sex.MAN).
                 map(p -> p.getFamily()).
                 collect(Collectors.toList());
+        System.out.println("Количество мужчин призывного возраста = " + personArmy.size());
 
+        List<Person> personWork = persons.stream().
+                filter(p -> p.getEducation() == Education.HIGHER && p.getAge() >= 18).
+                filter(p -> (p.getSex() == Sex.MAN && p.getAge() <= 65) || (p.getSex() == Sex.WOMEN && p.getAge() <= 60)).
+                sorted(Comparator.comparing(p -> p.getFamily())).
+                collect(Collectors.toList());
+        System.out.println("Количество трудоспособного населения = " + personWork.size());
+        if (!personWork.isEmpty()) {
+            System.out.println("Первый человек в списке = " + personWork.get(0));
+            System.out.println("Последний человек в списке = " + personWork.get(personWork.size() - 1));
+        }
     }
 }
